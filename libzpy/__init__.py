54d10dd220042a3e2d7387d3cc2cc2ebd46d00f6
mak <none@none>
2017-02-09 16:35:31 +0100

    fix importing

--- libzpy/__init__.py
+++ libzpy/__init__.py
@@ -1,24 +1,16 @@
 import sys,os
+#from contextlib import contextmanager
 
 def get_mydir():
     p = os.path.abspath(__file__)
     dir_path = os.path.dirname(p)
     return dir_path
 
+#@contextmanager
 def get_parser(m):
 
-    d = get_mydir()
-    if d not in sys.path:
-        sys.path = [d] + sys.path
-        while '/home/mak/tools/mtracker' in sys.path:
-            sys.path.remove('/home/mak/tools/mtracker')
+    mod = __import__('.'.join(['libzpy.modules',m]))
+    return getattr(mod.modules,m)
 
-    if 'libs' in sys.modules:
-        del sys.modules['libs']
-
-    if 'modules' in sys.modules:
-        del sys.modules['modules']
-
-    return getattr(__import__('.'.join(['modules',m])),m)
-
-
+def show_version(v):
+    return "%02d.%02d.%02d.%02d"%(v>>24,(v>>16)&0xff,(v>>8)&0xff,v&0xff)
